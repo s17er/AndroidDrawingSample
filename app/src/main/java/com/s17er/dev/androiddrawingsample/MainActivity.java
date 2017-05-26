bfc3501b9bc0d233fab1080fe067ed360073ea96
s17er <sinnerthesinner@gmail.com>
2017-05-26 15:24:28 +0900

    2nd commit

--- app/src/main/java/com/s17er/dev/androiddrawingsample/MainActivity.java
+++ app/src/main/java/com/s17er/dev/androiddrawingsample/MainActivity.java
@@ -16,7 +16,7 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
 
     @Bind(R.id.canvas)
-    FreeDrawView canvas;
+    CanvasView canvas;
 
     @Bind(R.id.seek_width)
     SeekBar widthSeek;
@@ -30,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        canvas.setMode(CanvasView.Mode.DRAW);
+        canvas.setDrawer(CanvasView.Drawer.PEN);
 
         widthSeek.setProgress(currentProgress);
         widthSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -46,33 +49,52 @@ public class MainActivity extends AppCompatActivity {
             public void onStopTrackingTouch(SeekBar seekBar) {
                 int range = WIDTH_MAX_PX - WIDTH_MIN_PX;
                 float px = WIDTH_MIN_PX + (range * currentProgress / 100);
-                canvas.setPaintWidthPx(px);
+                canvas.setPaintStrokeWidth(px);
             }
         });
     }
 
     @OnClick(R.id.button_color_black)
     public void onClickButtonBlack(View v) {
-        canvas.setPaintColor(Color.BLACK);
+        canvas.setMode(CanvasView.Mode.DRAW);
+        canvas.setPaintStrokeColor(Color.BLACK);
     }
     @OnClick(R.id.button_color_red)
     public void onClickButtonRed(View v) {
-        canvas.setPaintColor(Color.RED);
+        canvas.setMode(CanvasView.Mode.DRAW);
+        canvas.setPaintStrokeColor(Color.RED);
     }
     @OnClick(R.id.button_color_blue)
     public void onClickButtonBlue(View v) {
-        canvas.setPaintColor(Color.BLUE);
+        canvas.setMode(CanvasView.Mode.DRAW);
+        canvas.setPaintStrokeColor(Color.BLUE);
     }
+    @OnClick(R.id.button_eraser)
+    public void onClickButtonEraser(View v) {
+        canvas.setMode(CanvasView.Mode.ERASER);
+        canvas.setPaintStrokeColor(Color.BLUE);
+        //canvas.clearPaintStrokeColor();
+    }
+
     @OnClick(R.id.button_redo)
     public void onClickButtonRedo(View v) {
-        canvas.redoLast();
+        canvas.redo();
     }
     @OnClick(R.id.button_undo)
     public void onClickButtonUndo(View v) {
-        canvas.undoLast();
+        canvas.undo();
     }
     @OnClick(R.id.button_clear)
     public void onClickButtonClear(View v) {
         canvas.undoAll();
+    }
+
+    @OnClick(R.id.button_bg_white)
+    public void onClickButtonBgWhite(View v) {
+        canvas.setBaseColor(Color.WHITE);
+    }
+    @OnClick(R.id.button_bg_yellow)
+    public void onClickButtonBgYellow(View v) {
+        canvas.setBaseColor(Color.YELLOW);
     }
 }
